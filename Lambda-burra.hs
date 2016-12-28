3b1c3efda24b514d3e83fae34ee17bce7d70e7d2
Joseph-Student <24597827@e.ne.udo.edu.ve>
2016-12-27 21:38:26 -0400

    Agregando proyecto

---
+++ Lambda-burra.hs
@@ -0,0 +1,122 @@
+import Test.QuickCheck					
+import Data.Char							
+import Data.List
+
+data Value = Numeric Int | Sota | Caballo | Rey | As 
+						deriving (Eq, Show, Ord);
+
+data Suit = Oro | Copas | Espadas| Bastos	
+						deriving (Eq, Show);
+
+data Card =	Card { 
+									value :: Value,
+									suit :: Suit 
+								 } deriving (Eq, Show);
+
+newtype Hand = H Mallet deriving (Eq, Show);
+
+data Player = Lambda | You deriving (Eq, Show);
+
+type Mallet = [Card]
+
+mallet :: Mallet
+mallet = [card1,card25,card39,card17,card22,card16,card37,card28,card4,card30,card4,card29,card13,
+		card5,card31,card6,card27,card38,card9,card40,card21,card12,card32,card10,card2,card3,
+		card17,card8,card29,card10,card8,card23,card33,card34,card35,card12,card7,card18,card30,
+		card20,card11,card24,card26,card36,card14,card31,card19,card15,card5]
+
+card1 = Card As Oro
+card2 = Card (Numeric 2) Oro
+card3 = Card (Numeric 3) Oro
+card4 = Card (Numeric 4) Oro
+card5 = Card (Numeric 5) Oro
+card6 = Card (Numeric 6) Oro
+card7 = Card (Numeric 7) Oro
+card8 = Card Sota Oro
+card9 = Card Caballo Oro
+card10 = Card Rey Oro
+card11 = Card As Copas
+card12 = Card (Numeric 2) Copas
+card13 = Card (Numeric 3) Copas
+card14 = Card (Numeric 4) Copas
+card15 = Card (Numeric 5) Copas
+card16 = Card (Numeric 6) Copas
+card17 = Card (Numeric 7) Copas
+card18 = Card Sota Copas
+card19 = Card Caballo Copas
+card20 = Card Rey Copas
+card21 = Card As Espadas
+card22 = Card (Numeric 2) Espadas
+card23 = Card (Numeric 3) Espadas
+card24 = Card (Numeric 4) Espadas
+card25 = Card (Numeric 5) Espadas
+card26 = Card (Numeric 6) Espadas
+card27 = Card (Numeric 7) Espadas
+card28 = Card Sota Espadas
+card29 = Card Caballo Espadas
+card30 = Card Rey Espadas
+card31 = Card As Bastos
+card32 = Card (Numeric 2) Bastos
+card33 = Card (Numeric 3) Bastos
+card34 = Card (Numeric 4) Bastos
+card35 = Card (Numeric 5) Bastos
+card36 = Card (Numeric 6) Bastos
+card37 = Card (Numeric 7) Bastos
+card38 = Card Sota Bastos
+card39 = Card Caballo Bastos
+card40 = Card Rey Bastos
+
+
+hand1 = H [card14, card28, card3, card4, card38, card17, card8]
+hand2 = H [card26, card5, card32, card16]
+hand3 = H [card40, card5, card30]
+hand4 = H [card3, card31, card15]
+hand5 = H [card2]	
+hand6 = H [card15, card36, card23]
+hand7 = H [card27, card20, card13, card1, card40]
+hand8 = H [card37, card14, card21]
+hand9 = H [card29, card24, card18, card10]
+
+
+size :: Hand -> Int
+size (H xs)  = length xs 
+
+empty :: Hand
+empty = (H [])
+
+
+showSuit :: Card -> String								
+showSuit (Card _ s)
+	| s == Oro = "O"
+	| s == Copas = "C"
+	| s == Espadas = "E"
+	| s == Bastos = "B" 
+
+
+showValue :: Card -> String									
+showValue (Card v _)
+	| v == As = "A"
+	| v == Sota = "10"
+	| v == Caballo = "11"
+	| v == Rey = "12"
+	| v == (Numeric 2) = show 2
+	| v == (Numeric 3) = show 3
+	| v == (Numeric 4) = show 4
+	| v == (Numeric 5) = show 5
+	| v == (Numeric 6) = show 6
+	| v == (Numeric 7) = show 7
+
+
+searchSuitHand :: Hand -> Suit -> Bool
+searchSuitHand (H []) s = False
+searchSuitHand (H (Card _ su:xs)) s = if su == s then True else searchSuitHand (H xs) s
+
+loadUpMallet :: Mallet -> Suit -> Mallet
+loadUpMallet [] _ = []
+loadUpMallet (Card a su:xs) s = if s /= su then [Card a su]++loadUpMallet xs s else [Card a su]
+
+unir_mano :: Mallet -> Hand -> Hand
+unir_mano x (H cards) = (H (cards ++ x))
+
+turnolambda :: Mallet -> Hand -> Suit -> Hand
+turnolambda m h s = if searchSuitHand h s == False then	unir_mano (loadUpMallet m s) h else	empty
