cb66453d8fb85ee0c052c76e175b67f94ca18d19
Joseph-Student <24597827@e.ne.udo.edu.ve>
2017-01-11 21:00:29 -0400

    Agregando el modulo "Main".

--- LambdaBurra.hs
+++ LambdaBurra.hs
@@ -1,3 +1,8 @@
+module LambdaBurra
+( playGame
+, checkIndex
+, addCardToTable
+) where
 import Data.List
 import System.Random
 import System.IO.Unsafe
@@ -54,15 +59,6 @@ winRound :: Mallet -> Card
 winRound m = max (head m) $ head $ tail m
 
 ------------------------------------------------ Jugador ---------------------------------------------------------------
-
-------------------------------------------- Crea un mazo aleatorio -----------------------------------------------------
-randomMallet :: StdGen -> Mallet -> Mallet
-randomMallet gen m = let (r,g) = randomR (0, (sizeMallet m)-1) gen
-    in (m !! r: if sizeMallet m > 1 then randomMallet g $ delete (m !! r) m else [])
-
--------------------------------------- Crea las manos de los jugadores -------------------------------------------------
-createHands :: Mallet -> (Hand,Hand)
-createHands m = (H [x|x<-m,y<-[1,3..13],checkIndex x m y], H [x|x<-m,y<-[0,2..12],checkIndex x m y])
 
 -------------------------------------- Verifica el indice de la carta --------------------------------------------------
 checkIndex :: Card -> Mallet -> Int -> Bool
@@ -251,32 +247,5 @@ playGame hu hl m t p = do
             putStrLn "                      **Perdiste la Partida, Lambda se ha quedado sin cartas.**"
 
 -------------------------------------------------------- Main ----------------------------------------------------------
-
-main :: IO ()
-main = do
-    gen <- getStdGen
-    gen' <- newStdGen
-    putStrLn "                      Bienvenido al Juego Carga la Burra."
-    putStrLn ""
-    putStrLn ""
-    let rand_mallet = randomMallet gen mallet
-    let a = createHands rand_mallet
-    let table = addCardToTable (head $ drop 14 rand_mallet) []
-    let hand_lambda = fst a
-    let hand_you = snd a
-    let mallet_play = drop 15 rand_mallet
-    {- Prueba
-    let rand_mallet = mallet
-    let hand_lambda = hand1
-    let hand_you = hand5
-    let table = table_test
-    let mallet_play = rand_mallet
-    -}
-    putStrLn "                                  Mesa"
-    putStrLn $ "                                "++(showCard $ head table)
-    playGame hand_you hand_lambda mallet_play table You
-    putStrLn "                          Desea Jugar de Nuevo? S(Si) o N(No)."
-    r <- getLine
-    if r == "S" || r == "s" then main else putStrLn "                   Hasta luego"
 
 
---
+++ Main.hs
@@ -0,0 +1,42 @@
+module Main where
+
+import LambdaBurra
+import System.Random
+
+------------------------------------------- Crea un mazo aleatorio -----------------------------------------------------
+randomMallet :: StdGen -> Mallet -> Mallet
+randomMallet gen m = let (r,g) = randomR (0, (sizeMallet m)-1) gen
+    in (m !! r: if sizeMallet m > 1 then randomMallet g $ delete (m !! r) m else [])
+
+
+-------------------------------------- Crea las manos de los jugadores -------------------------------------------------
+createHands :: Mallet -> (Hand,Hand)
+createHands m = (H [x|x<-m,y<-[1,3..13],checkIndex x m y], H [x|x<-m,y<-[0,2..12],checkIndex x m y])
+
+main :: IO ()
+main = do
+    gen <- getStdGen
+    putStrLn "                      Bienvenido al Juego Carga la Burra."
+    putStrLn ""
+    putStrLn ""
+    let rand_mallet = randomMallet gen mallet
+    let a = createHands rand_mallet
+    let table = addCardToTable (head $ drop 14 rand_mallet) []
+    let hand_lambda = fst a
+    let hand_you = snd a
+    let mallet_play = drop 15 rand_mallet
+    {- Prueba
+    let rand_mallet = mallet
+    let hand_lambda = hand1
+    let hand_you = hand5
+    let table = table_test
+    let mallet_play = rand_mallet
+    -}
+    putStrLn "                                  Mesa"
+    putStrLn $ "                                "++(showCard $ head table)
+    playGame hand_you hand_lambda mallet_play table You
+    gen' <- newStdGen
+    putStrLn "                          Desea Jugar de Nuevo? S(Si) o N(No)."
+    r <- getLine
+    if r == "S" || r == "s" then main else putStrLn "                   Hasta luego"
+
